e0448ee96e4a76ac3d983060de9cd57d595fa1c2
Chuheng Zhang <zhangchuheng123@live.com>
2025-09-29 11:09:21 +0800

    update

--- examples/prediction_btc.py
+++ examples/prediction_btc.py
@@ -40,10 +40,10 @@ def plot_prediction(kline_df, pred_df):
 
 # 1. Load Model and Tokenizer
 tokenizer = KronosTokenizer.from_pretrained("NeoQuasar/Kronos-Tokenizer-base")
-model = Kronos.from_pretrained("NeoQuasar/Kronos-small")
+model = Kronos.from_pretrained("NeoQuasar/Kronos-base")
 
 # 2. Instantiate Predictor
-predictor = KronosPredictor(model, tokenizer, device="cuda:0", max_context=512)
+predictor = KronosPredictor(model, tokenizer, device="cuda:1", max_context=512)
 
 # 3. Prepare Data
 # df = pd.read_csv("./data/XSHG_5min_600977.csv")
